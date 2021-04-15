e01e1be448dcc4f62e5bc0356471e783a494803d
Gormm1980 <guillermosantamariarodrigo@hotmail.com>
2021-04-15 19:40:45 +0000

    show message on click

--- src/script.js
+++ src/script.js
@@ -1,15 +1,21 @@
+
 let who = ['The dog','My grandma','His turtle','My bird'];
 let action = ['ate','peed','crushed','broke'];
 let what = ['my homework', 'the keys', 'the car'];
 let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
+
+
 function getRandomExcuse() {
+    
     let whoRandom = Math.floor(Math.random()*who.length);
     let actionRandom =Math.floor(Math.random()*action.length);
     let whatRandom = Math.floor(Math.random()*what.length);
     let whenRandom = Math.floor(Math.random()*when.length);
     let excuse = who[whoRandom] + " " + action[actionRandom] + " " + what[whatRandom] + " " + when[whenRandom];
     console.log(excuse)
-    return excuse
+    document.getElementById('excuses').innerHTML= excuse;
 }
+
+
 
