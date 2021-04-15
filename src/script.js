e4ab483000f308a69975e041b5224fa3ca52bb2b
May-day19 <rencel.daycu@gmail.com>
2021-04-15 18:42:16 +0000

    wip

---
+++ src/script.js
@@ -0,0 +1,11 @@
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+function getRandomExcuse () {
+    let whoRandom = Math.floor(Math.random()*(who.length);
+    let actionRandom =
+    let whatRandom 
+    let whenRandom
+}
