769ea2fa1f98f1511acc968c50b832913c4bd36e
Diana Ware <dianasofia.ware@gmail.com>
2021-04-15 18:59:52 +0000

    Finished the getRandomExcuse function

--- src/script.js
+++ src/script.js
@@ -3,9 +3,13 @@ let action = ['ate','peed','crushed','broke'];
 let what = ['my homework', 'the keys', 'the car'];
 let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
-function getRandomExcuse () {
-    let whoRandom = Math.floor(Math.random()*(who.length);
-    let actionRandom =
-    let whatRandom 
-    let whenRandom
+function getRandomExcuse() {
+    let whoRandom = Math.floor(Math.random()*who.length);
+    let actionRandom =Math.floor(Math.random()*action.length);
+    let whatRandom = Math.floor(Math.random()*what.length);
+    let whenRandom = Math.floor(Math.random()*when.length);
+    let excuse = who[whoRandom] + " " + action[actionRandom] + " " + what[whatRandom] + " " + when[whenRandom];
+    console.log(excuse)
+    return excuse
 }
+
